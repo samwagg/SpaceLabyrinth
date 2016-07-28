8d82bbc43e2da639f435f30580480d7fe76ae545
Sam Waggoner <samwagg0583@gmail.com>
2016-07-27 17:56:46 -0700

    break out game logic to model class

---
+++ core/src/com/samwagg/gravity/main_game_module/GravityGameModel.java
@@ -0,0 +1,491 @@
+package com.samwagg.gravity.main_game_module;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.TiledDrawable;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.samwagg.gravity.GravityGame;
+import com.samwagg.gravity.ai.EnemySteeringAgent;
+import com.samwagg.gravity.main_game_module.game_objects.*;
+import com.samwagg.gravity.main_game_module.widgets.PauseMenu;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+
+/**
+ * Created by sam on 7/27/16.
+ */
+public class GravityGameModel {
+
+    private GravityGame game;
+    private GravityGameController controller;
+    private World world;
+
+    private GameCharacter character;
+    private List<Wall> walls;
+    private List<GravField> gravFields;
+    private List<GravField> currentGravFields;
+    private List<MovingWall> movingWalls;
+    private List<Explosion64> explosions;
+    private List<FinishSensor> endSensors;
+
+    private Vector2 gravVect;
+    
+    private boolean shipGone;    
+    private boolean explosionBegun;
+    private boolean restart;
+    private float crashForce;
+
+
+    private Map map;
+    private final float START_SCORE;
+
+    private float score;
+    private boolean levelFinished;
+
+    private final float WALL_WIDTH = 128;
+    private final float WALL_HEIGHT = 128;
+    private final float WORLD_WIDTH;
+    private final float WORLD_HEIGHT;        
+    private final float MAP_HEIGHT;
+    private final float MAP_WIDTH;
+
+    private float accumulator = 0;
+
+
+
+    public GravityGameModel(GravityGameController controller, final GravityGame game, Map map) {
+        this.game = game;
+        this.controller = controller;
+
+
+        MAP_HEIGHT = map.getTileArray().length;
+        MAP_WIDTH = map.getTileArray()[0].length;
+        WORLD_WIDTH = MAP_WIDTH * WALL_WIDTH;
+        WORLD_HEIGHT = MAP_HEIGHT * WALL_HEIGHT;
+
+        World.setVelocityThreshold(0);
+
+//		multiPlex = new InputMultiplexer();
+//		multiPlex.addProcessor(vSetter.getInputProcessor());
+
+        this.map = map;
+        START_SCORE = map.getInitScore();
+
+        world.setContactListener(new ForceListener());
+
+        resetLevel();
+    }
+
+    private void resetLevel() {
+        world= new World(new Vector2(0, 0), true);
+
+        walls = new ArrayList<Wall>();
+        gravFields = new ArrayList<GravField>();
+        endSensors = new ArrayList<FinishSensor>();
+        currentGravFields = new LinkedList<GravField>();
+        movingWalls = new LinkedList<MovingWall>();
+
+        explosions = new LinkedList<Explosion64>();
+
+        gravVect = new Vector2(0, -3);
+        world.setGravity(gravVect);
+
+        genGameObjects();
+        score = START_SCORE;
+        levelFinished = false;
+        shipGone = false;
+    }
+
+
+    /*
+ * Create all game objects from map member variable.
+ */
+    private void genGameObjects() {
+        
+        float xPos;
+        float yPos;
+        Map.GameTile currTile;
+        for (int i = 0; i < MAP_HEIGHT; i++) {
+
+            for (int j = 0; j < MAP_WIDTH; j++) {
+
+                xPos = j * WALL_WIDTH;
+                yPos = -i * WALL_HEIGHT;
+                //System.out.println("xpos = " + xPos + " and ypos = " + yPos);
+                currTile = map.getTileArray()[i][j];
+                if (currTile == null) continue;
+
+                switch (currTile) {
+                    case WALL:
+                        Rectangle block = extendTile(i, i, j, j, currTile, map.getTileArray());
+                        walls.add(new Wall(xPos, yPos - WALL_HEIGHT * (block.height - 1), block.width * WALL_WIDTH, block.height * WALL_HEIGHT,world,game.constants));
+                        break;
+                    case START:
+                        character = new GameCharacter(xPos, yPos,world,game.constants);
+                        System.out.println("i = " + i + " and j = " + j);
+                        character.updatePosition();
+                        System.out.println("here at char");
+                        break;
+                    case FORCE_RIGHT:
+                        gravFields.add(new GravField(xPos, yPos, 0,world,game.constants));
+                        break;
+                    case FORCE_UP:
+                        gravFields.add(new GravField(xPos, yPos, 90,world,game.constants));
+                        break;
+                    case FORCE_LEFT:
+                        gravFields.add(new GravField(xPos, yPos, 180,world,game.constants));
+                        break;
+                    case FORCE_DOWN:
+                        gravFields.add(new GravField(xPos, yPos, 270,world,game.constants));
+                        break;
+                    case END:
+                        endSensors.add(new FinishSensor(xPos, yPos,world,game.constants));
+                        break;
+                    case AI_START: // not implemented yet
+                        break;
+                    default:
+                        ;// Do nothing
+                }
+
+                if (currTile.isMovBlock()) {
+                    Rectangle movBlock = extendTile(i, i, j, j, currTile, map.getTileArray());
+                    makeMovBlock(i, j, (int) movBlock.width, (int) movBlock.height, currTile.getSpeed(), WALL_WIDTH, WALL_HEIGHT);
+                }
+            }
+        }
+
+    }
+
+    /*
+     * Search outward (either vertically or horizontally) from (i,j) for "RANGE" tiles, to define a MovingWall object
+     * centered at (i,j).
+     */
+    private void makeMovBlock(int i, int j, int width, int height, float speed, float WALL_WIDTH, float WALL_HEIGHT) {
+
+        if (map.getTileArray()[i + height][j] == Map.GameTile.RANGE_VERT
+                || map.getTileArray()[i - 1][j] == Map.GameTile.RANGE_VERT
+                || map.getTileArray()[i + height][j] == Map.GameTile.END_RANGE
+                || map.getTileArray()[i - 1][j] == Map.GameTile.END_RANGE) {
+
+            int rangeAbove = 0;
+            int rangeBelow = 0;
+
+            int k = 1;
+            while (true) {
+                if (map.getTileArray()[i + height - 1 + k][j] == Map.GameTile.RANGE_VERT) {
+                    rangeBelow += 1;
+                }
+                else if (map.getTileArray()[i + height - 1 + k][j] == Map.GameTile.END_RANGE) {
+                    rangeBelow += 1;
+                    break;
+                }
+                else break;
+                k++;
+            }
+
+            k = 1;
+            while (true) {
+                if (map.getTileArray()[i - k][j] == Map.GameTile.RANGE_VERT) {
+                    rangeAbove += 1;
+                }
+                else if (map.getTileArray()[i - k][j] == Map.GameTile.END_RANGE) {
+                    rangeAbove += 1;
+                    break;
+                }
+                else break;
+                k++;
+            }
+
+            movingWalls.add(new MovingWall(j * WALL_WIDTH, -i * WALL_HEIGHT - (height - 1) * WALL_HEIGHT, width * WALL_WIDTH, height * WALL_HEIGHT, new Vector2(0, 0), new Vector2(-i * WALL_HEIGHT - rangeBelow * WALL_HEIGHT - (height - 1) * WALL_HEIGHT, -i * WALL_HEIGHT + rangeAbove * WALL_HEIGHT - (height - 1) * WALL_HEIGHT),world,true, speed, game.constants));
+        }
+
+        else if (map.getTileArray()[i][j + width] == Map.GameTile.RANGE_HOR
+                || map.getTileArray()[i][j - 1] == Map.GameTile.RANGE_HOR
+                || map.getTileArray()[i][j + width] == Map.GameTile.END_RANGE
+                || map.getTileArray()[i][j - 1] == Map.GameTile.END_RANGE) {
+            int rangeRight = 0;
+            int rangeLeft = 0;
+
+            int k = 1;
+            while (true) {
+                if (map.getTileArray()[i][j + width - 1 + k] == Map.GameTile.RANGE_HOR) {
+                    rangeRight += 1;
+                }
+                else if (map.getTileArray()[i][j + width - 1 + k] == Map.GameTile.END_RANGE) {
+                    rangeRight += 1;
+                    break;
+                }
+                else break;
+                k++;
+            }
+
+            k = 1;
+            while (true) {
+                if (map.getTileArray()[i][j - k] == Map.GameTile.RANGE_HOR) {
+                    rangeLeft += 1;
+                }
+                else if (map.getTileArray()[i][j - k] == Map.GameTile.END_RANGE) {
+                    rangeLeft += 1;
+                    break;
+                }
+                else break;
+                k++;
+            }
+
+            movingWalls.add(new MovingWall(j * WALL_WIDTH, -i * WALL_HEIGHT - (height - 1) * WALL_HEIGHT, width * WALL_WIDTH, height * WALL_HEIGHT, new Vector2(j * WALL_WIDTH - rangeLeft * WALL_WIDTH, j * WALL_WIDTH + rangeRight * WALL_WIDTH), new Vector2(0, 0),world,false, speed, game.constants));
+        }
+    }
+
+    /*
+     * Recursively search outward from rectangular tile area represented by iLow, iHigh, jLow, jHigh, for tiles of
+     * type type, in order to traverse and record the dimensions of the area of like tiles.
+     */
+    private Rectangle extendTile(int iLow, int iHigh, int jLow, int jHigh, Map.GameTile type, Map.GameTile[][] map) {
+
+        boolean expandableRight = true, expandableLeft = true, expandableUp = true, expandableDown = true;
+
+        for (int i = iLow; i <= iHigh; i++) {
+            if (expandableRight && !(jHigh + 1 < map[0].length && map[i][jHigh + 1] == type)) {
+                expandableRight = false;
+
+            }
+            if (expandableLeft && !(jLow - 1 >= 0 && map[i][jLow - 1] == type)) {
+                expandableLeft = false;
+            }
+        }
+
+        if (expandableRight) jHigh++;
+        if (expandableLeft) jLow--;
+
+        for (int j = jLow; j <= jHigh; j++) {
+            if (expandableUp && !(iHigh + 1 < map.length && map[iHigh + 1][j] == type)) {
+                expandableUp = false;
+            }
+            if (expandableDown && !(iLow - 1 >= 0 && map[iLow - 1][j] == type)) {
+                expandableDown = false;
+            }
+        }
+
+        if (expandableUp) iHigh++;
+        if (expandableDown) iLow--;
+
+
+        if (!expandableUp && !expandableDown && !expandableRight && !expandableLeft) {
+            for (int i = iLow; i <= iHigh; i++) {
+                for (int j = jLow; j <= jHigh; j++) {
+                    map[i][j] = Map.GameTile.EMPTY;
+                }
+            }
+
+            return new Rectangle(jLow, iLow, jHigh - jLow + 1, iHigh - iLow + 1);
+        }
+        else {
+            //System.out.println("Recurse");
+            return extendTile(iLow, iHigh, jLow, jHigh, type, map);
+        }
+
+    }
+
+    public void doLogic(float delta) {
+
+        if (score == 0 && !explosionBegun) {
+            explosions.add(new Explosion64(character.getScreenX(), character.getScreenY()));
+            explosionBegun = true;
+            shipGone = true;
+            System.out.println("Beginning explosion");
+        }
+
+        for (Iterator<Explosion64> iter = explosions.iterator(); iter.hasNext();) {
+            Explosion64 exp = iter.next();
+            if (exp.done()) iter.remove();
+            System.out.println("Explosion done: " + exp.done() + "\nExplosion CurrentFrame: " + exp.getCurrentFrame());
+        }
+
+        if (score == 0 && explosions.isEmpty() || restart) {
+            System.out.println("explosions: " + explosions + "\nrestart = " + restart);
+            healthDepleted();
+            return;
+        }
+
+        float gravFieldDirection;
+        for (GravField field : currentGravFields) {
+            gravFieldDirection = field.getRotation() * (float) Math.PI / 180;
+            character.getBody().applyForceToCenter((float) (200 * Math.cos(gravFieldDirection)), (float) (200 * Math.sin(gravFieldDirection)), true);
+        }
+
+//        if (optionsClicked) {
+//
+//            vSetter.onInputTurnedOff();
+//            Gdx.input.setInputProcessor(stage);
+//
+//            switch (pauseMenu.displayIfPaused()) {
+//                case RESUME:
+//                    pauseMenu.reset();
+//                    optionsClicked = false;
+//                    System.out.println("resume pressed");
+//                    Gdx.input.setInputProcessor(vSetter.getInputProcessor());
+//                    break;
+//
+//                case RESTART:
+//                    restart = true;
+//                    break;
+//
+//                case MAIN_MENU:
+//                    System.out.println("Here at switch mainmenu");
+//                    game.mainMenuStartPressed(this);
+//                    break;
+//
+//                case REMAIN:
+//                    ;
+//            }
+//        }
+
+        doPhysicsStep(delta);
+//        if (countDown > 0) {
+//            displayDialog = true;
+//            countDown -= delta;
+//        }
+//        else if (!levelFinished && !optionsClicked) {
+//            Gdx.input.setInputProcessor(vSetter.getInputProcessor());
+//            doPhysicsStep(delta);
+//        }
+    }
+
+    public void setGravity(float x, float y) {
+        gravVect.x = x;
+        gravVect.y = y;
+        world.setGravity(gravVect);
+    }
+
+    public float getWallCrash() {
+        float lastCrashForce = crashForce;
+        crashForce = 0;
+        return lastCrashForce;
+    }
+
+    public List<GameObject> getObjects() {
+        
+    }
+
+    private void doPhysicsStep(float deltaTime) {
+        float frameTime = Math.min(deltaTime, 0.25f);
+        accumulator += frameTime;
+        while (accumulator >= 1 / 45f) {
+
+            world.step(1 / 45f, 6, 2);
+
+            if (score > 0 && !levelFinished) score--;
+
+            for (MovingWall wall : movingWalls) {
+                wall.move();
+            }
+            accumulator -= 1 / 45f;
+        }
+        character.updatePosition();
+    }
+
+    private void healthDepleted() {
+        resetLevel();
+    }
+
+    private void onWallCollision(float force) {
+
+        System.out.println(force);
+//        collisionSound.play();
+        crashForce = force;
+        score = score - GravityGame.CONTACT_DECREMENT * force < 0 ? 0 : score - GravityGame.CONTACT_DECREMENT * force;
+    }
+
+
+    /**
+     * ForceListener is a ContactListener that handles all contact events between
+     */
+    public class ForceListener implements ContactListener {
+
+
+        @Override
+        public void endContact(Contact contact) {
+            Fixture forceFix;
+            if (contact.getFixtureA().isSensor()) {
+                forceFix = contact.getFixtureA();
+            }
+            else if (contact.getFixtureB().isSensor()) {
+                forceFix = contact.getFixtureB();
+            }
+            else return;
+
+            GravField field = (GravField) forceFix.getUserData();
+            field.unlight();
+            currentGravFields.remove(field);
+        }
+
+        @Override
+        public void beginContact(Contact contact) {
+
+            Fixture fixA = contact.getFixtureA();
+            Fixture fixB = contact.getFixtureB();
+
+            Fixture forceFix;
+
+            if (fixA.getUserData().getClass().equals(GravField.class)) {
+                forceFix = contact.getFixtureA();
+            }
+            else if (fixB.getUserData().getClass().equals(GravField.class)) {
+                forceFix = contact.getFixtureB();
+            }
+            else if (fixA.getUserData().getClass().equals(FinishSensor.class) ||
+                    fixB.getUserData().getClass().equals(FinishSensor.class)) {
+                levelFinished = true;
+                return;
+            }
+            else return;
+
+            GravField field = (GravField) forceFix.getUserData();
+            field.light();
+            currentGravFields.add(field);
+        }
+
+
+        @Override
+        public void preSolve(Contact contact, Manifold oldManifold) {
+
+        }
+
+        @Override
+        public void postSolve(Contact contact, ContactImpulse impulse) {
+            Fixture fixA = contact.getFixtureA();
+            Fixture fixB = contact.getFixtureB();
+
+            float forceStren = impulse.getNormalImpulses()[0];
+
+            if (fixA.getUserData().getClass().equals(GameCharacter.class) &&
+                    fixB.getUserData().getClass().equals(Wall.class) ||
+                    fixA.getUserData().getClass().equals(Wall.class) &&
+                            fixB.getUserData().getClass().equals(GameCharacter.class)) {
+
+                onWallCollision(forceStren);
+
+            }
+            else if (fixA.getUserData().getClass().equals(GameCharacter.class) &&
+                    fixB.getUserData().getClass().equals(MovingWall.class) ||
+                    fixA.getUserData().getClass().equals(MovingWall.class) &&
+                            fixB.getUserData().getClass().equals(GameCharacter.class)) {
+                onWallCollision(forceStren);
+            }
+        }
+    }
+
+}
