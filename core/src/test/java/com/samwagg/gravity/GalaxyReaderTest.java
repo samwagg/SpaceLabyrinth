c47bac709534d58ef6f49219a0d845665eef0a00
Sam Waggoner <samwagg0583@gmail.com>
2016-09-27 16:20:13 -0700

    add test for reading valid Galaxy

--- core/src/test/java/com/samwagg/gravity/GalaxyReaderTest.java
+++ core/src/test/java/com/samwagg/gravity/GalaxyReaderTest.java
@@ -1,7 +1,57 @@
 package com.samwagg.gravity;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.files.FileHandle;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
 /**
  * Created by sam on 9/25/16.
  */
 public class GalaxyReaderTest {
+
+    private static final String VALID_GAL_DIR = "galaxies/valid_galaxy_7_levels";
+    private static final String INVALID_GAL_DIR_MISSING_LEVEL = "galaxies/invalid_galaxy_missing_level";
+    private static final String INVALID_GAL_DIR_MISSING_IMAGE = "galaxies/invalid_galaxy_missing_image";
+    private static final String INVALID_GAL_DIR_MISSING_JSON = "galaxies/invalid_galaxy_missing_image";
+
+    private  HeadlessApplication app = null;
+    private Game mockGame = Mockito.mock(Game.class);
+
+    /*
+     * Necessary because using Gdx.files requires a running application
+     */
+    @Before
+    public void before() {
+        app = new HeadlessApplication(mockGame);
+    }
+
+    @After
+    public void after() {
+        app.exit();
+        app = null;
+    }
+
+    @Test
+    public void validGalaxyDirectoryShouldReadWithoutException() {
+        GalaxyReader reader = new GalaxyReader();
+        FileHandle handle = Gdx.files.internal(VALID_GAL_DIR);
+        try {
+            reader.readGalaxy(handle);
+        }
+        catch (Exception exception) {
+            exception.printStackTrace();
+            fail("Expected no exception");
+        }
+    }
 }
